ed2988624e8ecc7dae207243e1ce1e3338e41b2d
HarpersDad <42348662+HarpersDad@users.noreply.github.com>
2021-12-01 18:15:36 -0500

    Health Script additions
    added death animation trigger to 0 health as well as an option to add death sound

--- NecroFarm/Assets/Scripts/Health.cs
+++ NecroFarm/Assets/Scripts/Health.cs
@@ -6,10 +6,19 @@ public class Health : MonoBehaviour
 {
     public int curHealth = int.MinValue;
     public int maxHealth = 100;
+    public int waitTime;
+
+    private Animator anim;
+
+    public string deathAnim;
 
     public string thisCharacter = "";
 
     public HealthBar healthBar;
+
+    private AudioSource sound;
+
+    public AudioClip clip;
 
     void Update()
     {
@@ -28,6 +37,26 @@ public class Health : MonoBehaviour
             DamagePlayer(10);
         }
         healthBar.SetHealth(curHealth);
+
+        if (curHealth <= 0)
+        {
+            anim = gameObject.GetComponent<Animator>();
+            sound = GetComponent<AudioSource>();
+            StartCoroutine(DeathWait());
+        }
+    }
+
+    IEnumerator DeathWait()
+    {
+        anim.Play(deathAnim);
+        sound.PlayOneShot(clip);
+        yield return new WaitForSeconds(waitTime);
+        DestroyUnit();
+    }
+
+    public void DestroyUnit()
+    {
+        Destroy(GameObject.Find(thisCharacter));
     }
 
     public void DamagePlayer(int damage)
